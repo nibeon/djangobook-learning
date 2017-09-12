0dc9a4cdf0ab9f0a6ff99c5714bef7e9ad0e8fc9
Vasya <xalabyda@yandex.ru>
2017-09-12 21:41:02 +0300

    подправил (вернул рабочий вариант) поиск по книгам

--- mysite/books/views.py
+++ mysite/books/views.py
@@ -7,14 +7,14 @@ def search_form(request):
 	return render(request, 'books/search_form.html')
 
 def search(request):
-    errors = []
+    error = False
     if 'q' in request.GET:
         q = request.GET['q']
         if not q:
-            errors.append('Enter a search term.')
-        elif len(q) > 20:
-            errors.append('Please enter at most 20 characters.')
+            error = True
         else:
             books = Book.objects.filter(title__icontains=q)
-            return render(request, 'search_results.html', {'books': books, 'query': q})
-    return render(request, 'search_form.html', {'errors': errors})
+            return render(request, 'books/search_results.html', {'books': books, 'query': q})
+    return render(request, 'books/search_form.html', {'error': error})
+
+    
