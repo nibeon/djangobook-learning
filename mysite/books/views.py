006be99296afb57464b14811ff47fa369473661e
nibeon <nibeon@gmail.com>
2017-09-12 16:31:59 +0300

    дошел до Tying Forms to Views (но вроде что-то сломал в поиске книг)

--- mysite/books/views.py
+++ mysite/books/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from books.models import Book
 
 # Create your views here.
 def search_form(request):
 	return render(request, 'books/search_form.html')
 
 def search(request):
-	if 'q' in request.GET and request.GET['q'] != '':
-		message = 'You searched for: %r' % request.GET['q']
-	else:
-		message = 'You submitted an empty form.'
-	return HttpResponse(message)
+    errors = []
+    if 'q' in request.GET:
+        q = request.GET['q']
+        if not q:
+            errors.append('Enter a search term.')
+        elif len(q) > 20:
+            errors.append('Please enter at most 20 characters.')
+        else:
+            books = Book.objects.filter(title__icontains=q)
+            return render(request, 'search_results.html', {'books': books, 'query': q})
+    return render(request, 'search_form.html', {'errors': errors})
--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,11 +1,32 @@
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+import datetime
+from mysite.forms import ContactForm    
+from django.core.mail import send_mail, get_connection
 
 def hello(request):
 	return HttpResponse("Hello world")
 
-#def displa_meta(request):
-#	values = request.META
-#	html = []
-#	for k in sorted(values):
-#		html.append('<tr><td>%s</td><td>%s</td></tr>' % (k, values[k]))
-#	return HttpResponse('<table>%s</table>' % '\n'.join(html))
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now        
+    return HttpResponse(html)
+
+def contact(request):
+	if request.method == 'POST':
+		form = ContactForm(request.POST)
+		if form.is_valid():
+			cd = form.cleaned_data
+			con = get_connection('django.core.mail.backends.console.EmailBackend')
+			send_mail(
+				cd['subject'],
+				cd['message'],
+				cd.get('email', 'noreply@example.com'),
+				['siteowner@example.com'],
+				connection=con
+			)
+			return HttpResponseRedirect('/contact/thanks')
+		else:
+			form = ContactForm()
+
+		return render(request, 'contact_form.html', {'form': form})
